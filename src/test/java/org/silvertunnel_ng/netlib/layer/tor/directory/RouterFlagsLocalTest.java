3719fe97bcb5a82eaabed0184baf892024d14730
Tobias Boese <tobias.boese@gmail.com>
2014-11-11 02:14:40 +0100

    fixing unittest for JDK 1.6

--- src/test/java/org/silvertunnel_ng/netlib/layer/tor/directory/RouterFlagsLocalTest.java
+++ src/test/java/org/silvertunnel_ng/netlib/layer/tor/directory/RouterFlagsLocalTest.java
@@ -20,11 +20,8 @@ package org.silvertunnel_ng.netlib.layer.tor.directory;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertNotNull;
-import static org.testng.AssertJUnit.assertNull;
 import static org.testng.AssertJUnit.assertTrue;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.silvertunnel_ng.netlib.tool.ConvenientStreamReader;
 import org.silvertunnel_ng.netlib.tool.ConvenientStreamWriter;
 import org.testng.annotations.Test;
